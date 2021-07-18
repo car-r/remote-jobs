209b1482cf50c930a935585ecab59cb3ca6637af
car-r <carson.ri@protonmail.com>
2021-07-18 11:32:37 -0700

    starting header component

---
+++ src/components/HeaderElements.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+    display: flex;
+    height: 7rem;
+`
+
+export const HeaderH1 = styled.h1`
+    font-size: 2rem;
+`
---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HeaderContainer, HeaderH1 } from '../HeaderElements'
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            <HeaderH1>Remote Job Finder</HeaderH1>
+        </HeaderContainer>
+    )
+}
+
+export default Header
